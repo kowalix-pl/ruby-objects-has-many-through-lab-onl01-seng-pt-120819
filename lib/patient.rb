435f4293b44925ae166458e8dfd1c0c838f819dc
kowalix007 <kowalix007@gmail.com>
2020-01-30 20:48:26 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+class Patient
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_appointment(doctor, date)
+   Appointment.new(date, self, doctor)
+  end 
+  
+  def appointments
+    appointment.all.select do |appointment|
+    appointment.patient == self
+  end
+  end 
+  
+end 
